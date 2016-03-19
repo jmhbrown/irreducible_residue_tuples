3b693a34e139e122939f5aa59ab275adc8b9aef8
jmhbrown <jm.howardbrown@gmail.com>
2016-03-18 22:58:51 -0700

    Check for irreducibility

--- __init__.py
+++ __init__.py
@@ -1,6 +1,10 @@
 import generation as g
+import irreducible as i
 from sympy.polys.polytools import Poly
 
 
 all_p = g.all_polynomials([0,1], 3)
-print(all_p)
+print("all:\n", all_p, "\n---")
+
+irreducible_p = i.get_irreducible(all_p)
+print("irreducible:\n", irreducible_p)
--- irreducible.py
+++ irreducible.py
@@ -2,5 +2,9 @@ from sympy.polys.polytools import Poly
 from sympy.abc import x
 
 # Filters a list based on irreducibility of elements.
-def filter_out_reducible(full_poly_list):
-    pass
+def get_irreducible(all_polynomials):
+    irreducible_polynomials = []
+    for poly in all_polynomials:
+        if poly.is_irreducible and poly.degree() > 0:
+            irreducible_polynomials.append(poly)
+    return irreducible_polynomials

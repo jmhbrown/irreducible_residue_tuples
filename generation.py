dd9f6de229252a90137820e67f175bd1ef0425b1
jmhbrown <jm.howardbrown@gmail.com>
2016-03-19 13:15:05 -0700

    Exclude constant polynomials.

--- generation.py
+++ generation.py
@@ -4,12 +4,14 @@ from itertools import product
 
 def get_irreducible_polynomials(coeff, num_terms):
     """
-    Generates a `list` in lexigraphical ordrer of all irreducible polynomials with:
+    Generates a `list` in lexigraphical ordrer of polynomials meeting the following conditions:
         * coefficients from `coeff`
         * degree less than `num_terms`
+        * non-constant (e.g. `x+1` is okay, `1` is not)
+        * irreducible
     """
 
-    irreducible_filter = lambda p: p.is_irreducible
+    irreducible_filter = lambda p: p.is_irreducible and Poly.degree(p) >= 1
     poly_lambda = lambda p: Poly(p, x, domain="FF(%i)" % len(coeff))
 
     irreducible = filter(irreducible_filter, map(poly_lambda, product(coeff, repeat=num_terms)))

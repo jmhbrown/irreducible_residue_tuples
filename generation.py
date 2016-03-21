677df29c65cd7d79e5504b0cd7745bea8c1f9b1b
jmhbrown <jm.howardbrown@gmail.com>
2016-03-21 06:07:32 -0700

    Restrict to polynomials with leading coefficient 1

--- generation.py
+++ generation.py
@@ -1,18 +1,20 @@
 from sympy.polys import Poly
 from sympy.abc import x
-from itertools import product
+from itertools import product,chain
 
 def get_irreducible_polynomials(coeff, num_terms):
     """
     Generates a `list` in lexigraphical ordrer of polynomials meeting the following conditions:
         * coefficients from `coeff`
-        * degree less than `num_terms`
-        * non-constant (e.g. `x+1` is okay, `1` is not)
+        * monic
+        * degree exactly `num_terms-1`
         * irreducible
     """
 
-    irreducible_filter = lambda p: p.is_irreducible and p.is_monic and Poly.degree(p) >= 1
-    poly_lambda = lambda p: Poly(p, x, domain="FF(%i)" % len(coeff))
+    irreducible_filter = lambda p: p.is_irreducible
+    # XXX - This assumes `1` will be nicely converted to identity element in the field.
+    #       Seems like a safe bet, but might be worth checking in on.
+    poly_lambda = lambda p: Poly(chain([1], p), x, domain="FF(%i)" % len(coeff))
 
-    irreducible = filter(irreducible_filter, map(poly_lambda, product(coeff, repeat=num_terms)))
+    irreducible = filter(irreducible_filter, map(poly_lambda, product(coeff, repeat=num_terms-1)))
     return list(irreducible)

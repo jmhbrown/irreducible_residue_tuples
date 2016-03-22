cd18d66c06d5863b104e8c22cde0c9deef242ea3
jmhbrown <jm.howardbrown@gmail.com>
2016-03-22 12:45:00 -0700

    Calculate the deviation from average frequency of residue sequences

--- __init__.py
+++ __init__.py
@@ -1,5 +1,6 @@
 import generation as g
 import residues as r
+import misc_calculations as misc
 import sys
 from sympy.polys.polytools import Poly
 from sympy.polys import Poly
@@ -11,18 +12,21 @@ from sympy.abc import x
 #
 # XXX - the elements given here as coefficients are just
 #       integers, which might behave oddly.
-coeff = [0,1,2]
+coeff = [0,1]
 if len(sys.argv) > 1:
     num_terms = int(sys.argv[1])
 else:
     num_terms = 2
 sequence_length = 2
-modulus_poly = Poly(x, x, domain="FF(%i)" % len(coeff))
+modulus_poly = Poly(x**2+x+1, x, domain="FF(%i)" % len(coeff))
 
 print("coeff: ", coeff, ", num_terms: ", num_terms,"\n---")
 
 irreducible_p = g.get_irreducible_polynomials(coeff, num_terms)
 print("# of irreducible polynomials:\t", len(irreducible_p), "\n---")
+
+avg_sequence_freq = misc.average_sequence_frequency(modulus_poly,len(coeff), len(irreducible_p), sequence_length)
+print("expected average residue sequence frequency:\t", avg_sequence_freq)
 
 residues_p = r.get_residues(irreducible_p, modulus_poly)
 # print("residues:\n", residues_p, "\n---")
@@ -32,3 +36,6 @@ print("residue counts:\n", residue_counts, "\n---")
 
 sequence_counts = r.get_sequence_frequencies(residues_p, sequence_length)
 print("sequence frequencies:\n", sequence_counts, "\n---")
+
+deviation_from_average = misc.get_deviation_from_sequence_frequency(sequence_counts,avg_sequence_freq, num_terms, len(coeff))
+print("sequence frequecy deviations from the average:\n", deviation_from_average, "\n---")
